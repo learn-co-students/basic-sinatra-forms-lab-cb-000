fdd0083b1960a975450e19ef8aea94e033691a79
Juha  Bach <cdalsoniii@gmail.com>
2017-02-27 00:35:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @pos = ["Team Name: ","Coach: ","Point Guard: ","Shooting Guard: ","Power Forward: ","Small Forward: ","Center: "]
+    data = params.values
+    @pos = @pos.map.with_index {|v,i| v + data[i]}
+    erb :team
+  end
 
 end

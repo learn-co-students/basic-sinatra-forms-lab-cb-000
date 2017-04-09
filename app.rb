b1b444b15e2894bda72a4e5efdbbbecd7e469edf
David Ordovskiy <david.ordovsky@gmail.com>
2017-04-08 21:23:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,27 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @mappings = {
+      "name"=>"Team Name",
+      "coach"=>"Coach", 
+      "pg"=> "Point Guard", 
+      "sg"=>"Shooting Guard", 
+      "pf"=>"Power Forward", 
+      "sf"=>"Small Forward", 
+      "c"=>"Center"
+    }
+
+    @team = params.dup.map do |k, v|
+      [@mappings[k], v]
+    end.to_h
+
+    erb :team
+  end
 
 end

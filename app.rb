bf90e5c656b3ea6c86f9713c343efbe1bba59d90
Jacob Wald <schoen575@gmail.com>
2017-04-24 20:05:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  helpers do
+    def capitalize_words(string)
+      string.split(/[ _]/).map(&:capitalize).join(' ')
+    end
+  end
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name = params[:name]
+    @team_info = {
+      coach: params[:coach],
+      point_guard: params[:pg],
+      shooting_guard: params[:sg],
+      small_forward: params[:sf],
+      power_forward: params[:pf],
+      center: params[:c]
+    }
+    erb :team
+  end
 
 end

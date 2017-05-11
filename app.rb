2a194f37e0cb56480e642bdff28e069c01693f95
Tyler Bristol <tahbristol@gmail.com>
2017-05-11 02:35:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
 
+    erb :newteam
+  end
+
+  post '/team' do
+
+    @members = []
+     params.each do |key, value|
+          @members << value
+        end
+
+    erb :team
+  end
 end

5f125f2e9d8de74c4319814f53d1433b722657b5
petitlapin86 <skinnerpa86@yahoo.com>
2018-02-18 17:07:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do #Create a route that responds to a GET request at /newteam
+    erb :newteam
+  end
+
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
+  end
 
 end

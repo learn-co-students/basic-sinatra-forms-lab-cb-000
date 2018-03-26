bb6f56f3b80db089b0e88c1da78939815dee2418
stritch.dev <stritch.dev@gmail.com>
2018-03-26 14:48:25 -0500

    works in browser but does not pass tests

--- app.rb
+++ app.rb
@@ -7,16 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/team' do 
-    # params.each { |key, value| self.instance_variable_set("@#{key}=", value) }
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
+     @name = params['name']
+     @coach = params[:coach]
+     @pg = params[:pg]
+     @sg = params[:sg]
+     @pf = params[:pf]
+     @sf = params[:sf]
+     @c = params[:c]
+     erb :team
 
-    erb :team
   end
 
 end

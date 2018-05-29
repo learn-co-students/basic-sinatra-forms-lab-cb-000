0536a3f1c0e2f364ce1b405b2e33c00da8e1ded9
Jen Poole <contact.vandepool@gmail.com>
2018-05-29 02:08:50 +0000

    GET /newteam

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-
+  # Create a route that responds to a GET request at /newteam
+  # sends a 200 status code
+  # renders basketball team form
+  get '/newteam' do
+    erb :newteam
+  end
 end

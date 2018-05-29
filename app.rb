bfe64ccb5e6fa775096a6d6ea1fe5026ef64fc02
Jen Poole <contact.vandepool@gmail.com>
2018-05-29 02:18:52 +0000

    add form and route form submission to /team

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
   get '/newteam' do
     erb :newteam
   end
+
+  # Create a route that responds to a POST request at /team
+  # Have the form send a POST request to this route.
+  # Upon submission, pass the submitted data to the team.erb template.
+  post '/team' do
+    erb :team
+  end
+
 end
